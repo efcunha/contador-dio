cb0ed4012b0a943e96978b762cade74b154a0a27
Edson Fernandes Cunha <52961166+efcunha@users.noreply.github.com>
2022-05-23 16:50:41 -0400

    curso update

--- js/app.js
+++ js/app.js
@@ -1,29 +1,38 @@
-// set inital value to zero
+// Valor inicial 0
 let count = 0;
-// select value and buttons
+// Selecione os botões para gerar o valor
 const value = document.querySelector("#value");
 const btns = document.querySelectorAll(".btn");
 
 btns.forEach(function (btn) {
   btn.addEventListener("click", function (e) {
     const styles = e.currentTarget.classList;
-    if (styles.contains("decrease")) {
-      count--;
-    } else if (styles.contains("increase")) {
-      count++;
-    } else {
-      count = 0;
-    }
-
-    if (count > 0) {
-      value.style.color = "green";
-    }
-    if (count < 0) {
-      value.style.color = "red";
-    }
-    if (count === 0) {
-      value.style.color = "#222";
-    }
+      if (styles.contains("decrease")) {
+        count--;
+      } else if (styles.contains("increase")) {
+        count++;
+      } else {
+        count = 0;
+      }
+      if (count > 0) {
+        value.style.color = "green";
+      }
+      if (count < 0) {
+        value.style.color = "red";
+      }
+      if (count === 0) {
+        value.style.color = "#222";
+      }
     value.textContent = count;
+
+    const disableButton = document.querySelectorAll(".btn"); {
+      disableButton.forEach(function (btn) {
+        if (count <= -10 || count >= 10) {
+          btn.disabled = true;
+        } else {
+          btn.disabled = false;
+        }
+      });
+    }
   });
 });
